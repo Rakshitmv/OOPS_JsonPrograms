8627b63d04bf6d57fe03d1ef9b41deb8db7b8f62
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-07 12:29:07 +0530

    [Rakshit] Add: UC1_Json Inventory Data Management of Rice,pusles and wheat

---
+++ OOPSJsonPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSJsonPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"E:\Git Programs\OOPS_JsonPrograms\OOPS_JsonPrograms\OOPSJsonPrograms\InventoryData.json");
+            Console.ReadKey();
+
+        }
+    }
+}
