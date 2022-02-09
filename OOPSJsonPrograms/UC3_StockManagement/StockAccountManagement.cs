a2cbf59a7d151b020c3a10cdb90831cae8eb2513
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-09 22:56:05 +0530

    [Rakshit] Add: UC3 Stock Account Management completed

---
+++ OOPSJsonPrograms/UC3_StockManagement/StockAccountManagement.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSJsonPrograms.UC3_StockManagement
+{
+    public class StockAccountManagement
+    {
+        public void ReadData(String filePath)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    var json = reader.ReadToEnd();
+                    var itemsPresent = JsonConvert.DeserializeObject<List<StocksInventory>>(json);
+                    Console.WriteLine("Name\t" + "Price\t" + "NoOfShares\t" + "Total amount");
+                    int totalValue = 0, amount = 0;
+                    foreach (var data in itemsPresent)
+                    {
+                        amount = data.SharePrice * data.NumberOfShares;
+                        totalValue += amount;
+                        Console.WriteLine(data.StockName + "\t" + data.SharePrice + "\t" + data.NumberOfShares + "\t" + amount);
+                    }
+                    Console.WriteLine("\nTotal stock report" + totalValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ OOPSJsonPrograms/UC3_StockManagement/StocksInventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSJsonPrograms.UC3_StockManagement
+{
+    public class StocksInventory
+    {
+        public string StockName { get; set; }
+        public int NumberOfShares { get; set; }
+        public int SharePrice { get; set; }
+    }
+}
